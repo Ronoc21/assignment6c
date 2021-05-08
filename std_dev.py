3068e8b61b7ce82540291b4b94c24a6084115667
Conor <conor.okeefe.2121@gmail.com>
2021-05-07 22:41:22 -0700

    std_dev.py

---
+++ std_dev.py
@@ -0,0 +1,20 @@
+#Name: Conor Smith
+#Date: 05/05/21
+#Description: Creates person objects with names and ages, finds standard deviation of their ages
+
+class Person:
+    """"""
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+    def get_age(self):
+        return self._age
+
+    def std_dev(Person)
+        return [s for s in age]
+
+#p1 = Person("Kyoungmin", 73)
+#p2 = Person("Mercedes", 24)
+#p3 = Person("Beatrice", 48)
+#person_list = [p1, p2, p3]
+#answer = std_dev(person_list)
